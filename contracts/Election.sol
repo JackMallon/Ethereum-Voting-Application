3639a6226e4caa8564adf36557c9cb5ccb03ffca
Jack Mallon <jack.mallonk@gmail.com>
2020-01-30 15:44:34 +0000

    First commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.21;
+
+contract Election {
+    // Store Candidate
+    // Read Candidate
+    string public candidate;
+    // Constructor
+    constructor() public{
+        candidate = "Candidate 1";
+    }
+}
